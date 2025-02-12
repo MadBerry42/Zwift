c5dc336dd433bd2eeb1895d562c1975a55a3bfb0
Maddalena Berruti <maddy.berruti@gmail.com>
2025-02-12 18:07:25 +0100

    Insert P info by keyboard

--- Filtering_power.py
+++ Filtering_power.py
@@ -7,12 +7,20 @@ from openpyxl import load_workbook
 import Extract_HR_Features
 
 # Participant details
-ID = 16
+'''ID = 16
 Gender = 1 #0 for males, 1 for females
-Age = 25
+Age = 24
 Weight = 59
 Height = 173
-max_HR = 195
+max_HR = 195'''
+
+ID = input("What is participant's ID?")
+ID = int(ID)
+Gender = input(f"What is participant {ID}'s gender? (0 = male, 1 = female)")
+Age = input(f"What is participant {ID}'s age?")
+Weight = input(f"What is participant {ID}'s weight?")
+Height = input(f"What is participant {ID}'s height?")
+max_HR = input(f"What is participant {ID}'s max HR?")
 
 
 if ID < 10:
@@ -27,19 +35,24 @@ window_size = 15
 # Handcycle
 #-----------------------------------------------------------------------------------------------------------------------------------------
 setup = "handcycle"
-data = pd.read_csv(f"C:\\Users\\maddalb\\Desktop\\git\\Zwift\\Acquisitions\\Protocol\\{ID}\\Zwift\\{ID}_{setup}_protocol.csv")
-data = data[300:840]
+data = pd.read_csv(f"C:\\Users\\maddy\\Desktop\\Roba seria\\II ciclo\\Tesi\\Acquisitions\\Input to models\\Originals\\{ID}_{setup}_protocol.csv")
+beginning = 300 # 300 for the first part of the workout, 840 for the second part
+final = 1380 # 840 for the first part, 1380 for the second part
+data = data[beginning : final]
 power_hc = np.array(data["Power"])
 HR = np.array(data["Heart Rate"])
 RPE = np.array(data["RPE"])
+if RPE[0] != 12:
+    RPE[0] = 12
 RPE = RPE.astype(int)
+cadence = np.array(data["Cadence"])
 # Moving average filter
 window = np.ones(window_size) / window_size
 window = window.flatten()
 power_hc = power_hc.flatten()
 power_hc = np.convolve(power_hc, window, mode = "same")
 # Plotting the signal
-t = np.linspace(300, 840, num = len(power_hc))
+t = np.linspace(beginning, final, num = len(power_hc))
 plt.figure()
 plt.plot(t, data["Power"])
 plt.plot(t, power_hc)
@@ -57,17 +70,17 @@ features_hr = Extract_HR_Features.get_features_from_hr_signal(HR)
 # Bicycle
 #-----------------------------------------------------------------------------------------------------------------------------------------
 setup = "bicycle"
-data = pd.read_csv(f"C:\\Users\\maddalb\\Desktop\\git\\Zwift\\Acquisitions\\Protocol\\{ID}\\Zwift\\{ID}_{setup}_protocol.csv", usecols = ["Power"])
-data = data[300:840]
+data = pd.read_csv(f"C:\\Users\\maddy\\Desktop\\Roba seria\\II ciclo\\Tesi\\Acquisitions\\Input to models\\Originals\\{ID}_{setup}_protocol.csv", usecols = ["Power"])
+data = data[beginning : final]
 power_bc = np.array(data)
 # Moving average filter
 window = np.ones(window_size) / window_size
 window = window.flatten()
 power_bc = power_bc.flatten()
-power_bc = np.convolve(power_bc, window, mode = "same")
+data_filtered_bc = np.convolve(power_bc, window, mode = "same")
 
-data_filtered = power_hc[1: -1]
-data_filtered_bc = power_bc[1: -1]
+'''data_filtered = power_hc[1: -1]
+data_filtered_bc = data_filtered_bc[1: -1]'''
 
 
 # Two subplots
@@ -80,7 +93,7 @@ ax[1].set_title("Filtered signal")'''
 # One plot, signals overlapped
 plt.figure()
 plt.plot(t, data["Power"])
-plt.plot(t[1:-1], data_filtered_bc)
+plt.plot(t, data_filtered_bc)
 plt.xlabel("Time [s]")
 plt.ylabel("Power output [W]")
 plt.title(f"Subject {ID}, {setup}")
@@ -106,7 +119,7 @@ Gender = np.ones(540) * Gender'''
     writer.writerows(rows)'''
 
 # Create an excel file
-path = f"Acquisitions\\Protocol\\Processed data\\Input to model" # Location of the output file
+path = f"C:\\Users\\maddy\\Desktop\\Roba seria\\II ciclo\\Tesi\\Acquisitions\\Input to models" # Location of the output file
 Col_A = [' ', 'Gender', 'Age', 'Weight', 'Height', 'max_HR']
 Col_B = [' ', Gender, Age, Weight, Height, max_HR]
 Col_A.extend([' '] * (len(power_hc) - len(Col_A)))
@@ -114,7 +127,7 @@ Col_B.extend([' '] * (len(power_hc) - len(Col_B)))
 
 writer = pd.ExcelWriter(f'{path}\\{ID}_input_file.xlsx', engine = "openpyxl")
 wb = writer.book
-df = pd.DataFrame({'P info': Col_A, ' ' : Col_B, 'Heart Rate': HR, 'RPE': RPE, 'Power hc': power_hc, 'Power bc': power_bc})
+df = pd.DataFrame({'P info': Col_A, ' ' : Col_B, 'Heart Rate': HR, 'RPE': RPE, 'Cadence': cadence, 'Power hc': power_hc, 'Power bc': data_filtered_bc})
 features_hr = pd.DataFrame([features_hr])
 df = pd.concat([df, features_hr], axis = 1)
 df = df.fillna(' ')
