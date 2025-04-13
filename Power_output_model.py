4a483831ffd57b05f53910bca34c58562a529c19
Maddalena Berruti <maddy.berruti@gmail.com>
2025-04-14 00:00:43 +0200

    Added simple model

--- Power_output_model.py
+++ Power_output_model.py
@@ -3,11 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.lines import Line2D
 import math
+# Linear regression for predicting coefficients
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
+
 
 participants = [0, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16]
 path = r"C:\Users\maddy\Desktop\NTNU\Julia Kathrin Baumgart - Protocol Data"
 mode = "raw"
 plot_hr = "No"
+simple_model = "Yes"
 
 class Plots():
     def __init__(self):
@@ -55,36 +61,21 @@ class Plots():
 draw_plots = Plots()
 
 # Create P info columns
-'''members = { "000":{"Age": 25, "Height": 160, "Weight": 58, "Gender": 1, "FTP": 49},
-            "002":{"Age": 26, "Height": 177, "Weight": 75, "Gender": 0, "FTP": 78},
-            "003":{"Age": 22, "Height": 180, "Weight": 70, "Gender": 0, "FTP": 51},
-            "004":{"Age": 22, "Height": 186, "Weight": 80, "Gender": 0, "FTP": 47},
-            "006":{"Age": 23, "Height": 174, "Weight": 87, "Gender": 1, "FTP": 51},
-            "007":{"Age": 23, "Height": 183, "Weight": 70, "Gender": 0, "FTP": 55},
-            "008":{"Age": 23, "Height": 190, "Weight": 82, "Gender": 0, "FTP": 82},
-            "009":{"Age": 32, "Height": 185, "Weight": 96, "Gender": 0, "FTP": 62},
-            "010":{"Age": 24, "Height": 160, "Weight": 56, "Gender": 1, "FTP": 48},
-            "011":{"Age": 28, "Height": 176, "Weight": 67, "Gender": 0, "FTP": 60},
-            "012":{"Age": 28, "Height": 184, "Weight": 70, "Gender": 0, "FTP": 87},
-            "013":{"Age": 25, "Height": 178, "Weight": 66, "Gender": 0, "FTP": 62},
-            "015":{"Age": 21, "Height": 176, "Weight": 73, "Gender": 0, "FTP": 60},
-            "016":{"Age": 24, "Height": 173, "Weight": 59, "Gender": 1, "FTP": 37},
-            }'''
 
-members = { "000":{"Age": 25, "Height": 160, "Weight": 58, "Gender": 1, "FTP": 49, "Activity": [0, 0, 60*7, 16*60*7]},
-        "002":{"Age": 26, "Height": 177, "Weight": 75, "Gender": 0, "FTP": 78, "Activity": [0, 4*45, 6*30, 4*60*7]},
-        "003":{"Age": 22, "Height": 180, "Weight": 70, "Gender": 0, "FTP": 51, "Activity": [4*60, 7*15, 3*30, 8*60*7]},
-        "004":{"Age": 22, "Height": 186, "Weight": 80, "Gender": 0, "FTP": 47, "Activity": [3*90, 1*5, 5*30, 6*60*7]},
-        "006":{"Age": 23, "Height": 174, "Weight": 87, "Gender": 1, "FTP": 51, "Activity": [2*40, 0, 7*120, 9*60*7 ]},
-        "007":{"Age": 23, "Height": 183, "Weight": 70, "Gender": 0, "FTP": 55, "Activity": [0, 0, 60*7, 16*60*7]}, # Unsure about the sitting time
-        "008":{"Age": 23, "Height": 190, "Weight": 82, "Gender": 0, "FTP": 82, "Activity": [4*90, 0, 7*30, 8*60*7]},
-        "009":{"Age": 32, "Height": 185, "Weight": 96, "Gender": 0, "FTP": 62, "Activity": [0, 0, 5*60, 12*60*7]},
-        "010":{"Age": 24, "Height": 160, "Weight": 56, "Gender": 1, "FTP": 48, "Activity": [5*60, 2*60, 7*30, 10*60*7]}, # Time spent for activity is grossly estimated
-        "011":{"Age": 28, "Height": 176, "Weight": 67, "Gender": 0, "FTP": 60, "Activity": [0, 0, 7*40, 12*60*7]},
-        "012":{"Age": 28, "Height": 184, "Weight": 70, "Gender": 0, "FTP": 87, "Activity": [0, 0, 4*20, 7*45, 10*60*7]},
-        "013":{"Age": 25, "Height": 178, "Weight": 66, "Gender": 0, "FTP": 62, "Activity": [1*60, 1*60, 3*35, 4*60*7]},
-        "015":{"Age": 21, "Height": 176, "Weight": 73, "Gender": 0, "FTP": 60, "Activity": [4*80, 6*120, 7*8*60, 3*60*7]},
-        "016":{"Age": 24, "Height": 173, "Weight": 59, "Gender": 1, "FTP": 37, "Activity": [2*45, 2*30, 7*60, 9*60*7]},
+members = { "000":{"Age": 25, "Height": 160, "Weight": 58, "Gender": 1, "FTP": 49, "RPE": [[13, 11, 12], [12, 11, 13]], "Activity": [0, 0, 7*60]},
+        "002":{"Age": 26, "Height": 177, "Weight": 75, "Gender": 0, "FTP": 78, "RPE": [[9, 12, 14], [9, 11, 13]], "Activity": [0, 4*45, 6*30]},
+        "003":{"Age": 22, "Height": 180, "Weight": 70, "Gender": 0, "FTP": 51, "RPE": [[10, 12, 15], [8, 10, 12]],"Activity": [4*60, 7*15, 5*30]},
+        "004":{"Age": 22, "Height": 186, "Weight": 80, "Gender": 0, "FTP": 47, "RPE": [[11, 12, 12], [10, 11, 12]],"Activity": [3*90, 1*5*60, 5*30]},
+        "006":{"Age": 23, "Height": 174, "Weight": 87, "Gender": 1, "FTP": 51, "RPE": [[10, 11, 12], [10, 11, 10]],"Activity": [2*40, 0, 7*120]},
+        "007":{"Age": 23, "Height": 183, "Weight": 70, "Gender": 0, "FTP": 55, "RPE": [[11, 11, 12], [8, 9, 11]],"Activity": [0, 0, 7*60]},
+        "008":{"Age": 23, "Height": 190, "Weight": 82, "Gender": 0, "FTP": 82, "RPE": [[12, 12, 14], [10, 12, 14]],"Activity": [4*90, 0, 7*30]},
+        "009":{"Age": 32, "Height": 185, "Weight": 96, "Gender": 0, "FTP": 62, "RPE": [[9, 11, 14], [10, 11, 14]],"Activity": [0, 0, 5*60]},
+        "010":{"Age": 24, "Height": 160, "Weight": 56, "Gender": 1, "FTP": 48, "RPE": [[9, 11, 13], [9, 10, 11]],"Activity": [5*60, 2*60, 7*30]}, # Time spent for activity is grossly estimated
+        "011":{"Age": 28, "Height": 176, "Weight": 67, "Gender": 0, "FTP": 60, "RPE": [[11, 10, 13], [10, 11, 13]],"Activity": [0, 0, 7*40]},
+        "012":{"Age": 28, "Height": 184, "Weight": 70, "Gender": 0, "FTP": 87, "RPE": [[10, 11, 12], [10, 11, 12]],"Activity": [10, 4*20, 7*45]},
+        "013":{"Age": 25, "Height": 178, "Weight": 66, "Gender": 0, "FTP": 62, "RPE": [[10, 11, 13], [11, 11, 13]],"Activity": [1*60, 1*60, 3*35]},
+        "015":{"Age": 21, "Height": 176, "Weight": 73, "Gender": 0, "FTP": 60, "RPE": [[9, 10, 11], [10, 11, 13]],"Activity": [4*80, 6*2*60, 7*8*60]},
+        "016":{"Age": 24, "Height": 173, "Weight": 59, "Gender": 1, "FTP": 37, "RPE": [[9, 10, 11], [7, 9, 10]],"Activity": [2*45, 2*30, 7*60]},
         }
 
 fig1, axs1 = plt.subplots(4, 4)
@@ -135,7 +126,7 @@ for i, ID in enumerate(participants):
         # Predicting P_hc and P_bc
     #--------------------------------------------------------------------------------------------------------------------------------------------
     # Find coefficients which predict Pbc and Phc based on % Peak HR
-    perc_hr = np.linspace(50, 80, 100)
+    perc_hr = np.linspace(1, 100, 100)
     P_hc_pred = slopes[i, 0] * perc_hr + intercepts[i, 0]
     P_bc_pred = slopes[i, 1] * perc_hr + intercepts[i, 1]
 
@@ -143,7 +134,7 @@ for i, ID in enumerate(participants):
     slopes_model[i], intercepts_model[i] = draw_plots.plot_graphs(P_hc_pred, P_bc_pred, "Handcycle: predicted Power Outputs", 
                             "Predicted hc [W]", "Predicted bc [W]", participants[i], # Labels for x and y axes
                             i, fig3, axs3, color_coded = "No") # Plot properties
-    # axs3[i].set_xlim([40, 100])
+    axs3[i].set_xlim([0, 100])
     axs3[i].set_ylim([50, 250])
     
 #--------------------------------------------------------------------------------------------------------------------------------------------------
@@ -179,6 +170,8 @@ for i, ID in enumerate(participants):
     axs5[0].scatter(intercepts_model[i], slopes_model[i], color = c)
     axs5[0].legend(["Females", "Males"], loc = 'upper right')
     axs5[0].set_title("Gender")
+    axs5[0].set_xlabel("Beta")
+    axs5[0].set_ylabel("Alpha")
 
         # Color code by age
     #----------------------------------------------------------------------------------------------------------------------------------
@@ -206,6 +199,8 @@ for i, ID in enumerate(participants):
     legend_handles = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
     axs5[1].legend(legend_handles, legend_labels, loc='upper right')
     axs5[1].set_title("Age")
+    axs5[1].set_xlabel("Beta")
+    axs5[1].set_ylabel("Alpha")
 
 
     # Color code by height
@@ -232,6 +227,9 @@ for i, ID in enumerate(participants):
     legend_handles = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
     axs5[2].legend(legend_handles, legend_labels, loc='upper right')
     axs5[2].set_title(f"{feature}")
+    axs5[2].set_xlabel("Beta")
+    axs5[2].set_ylabel("Alpha")
+
 
     # Color code by height
 #-----------------------------------------------------------------------------------------------------------------------------------------
@@ -258,12 +256,42 @@ for i, ID in enumerate(participants):
     legend_handles = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
     axs5[idx].legend(legend_handles, legend_labels, loc='upper right')
     axs5[idx].set_title(f"{feature}")
+    axs5[idx].set_xlabel("Beta")
+    axs5[idx].set_ylabel("Alpha")
+
+        # Color code by IPAQ result
+    #------------------------------------------------------------------------------------------------------------------------------------
+    feature = "Level of physical activity"
+    idx = 4
+    # Save the number of minutes per week dedicated to each level of activity
+    low_activity = members[f"{ID}"]["Activity"][2]
+    moderate_activity = members[f"{ID}"]["Activity"][1]
+    vigorous_activity = members[f"{ID}"]["Activity"][0]
+
+    ipaq = 3.3 * low_activity + 4 * moderate_activity + 8 * vigorous_activity 
+
+    if ipaq <= 600:
+        c = "green"
+    elif ipaq > 600 and ipaq <= 1500:
+        c = "yellow"
+    elif ipaq > 1500:
+        c = "red"
+
+    axs5[idx].scatter(intercepts_model[i], slopes_model[i], color = c)
+
+    legend_labels = ["Low", "Moderate", "Vigorous"]
+    legend_colors = ["green", "yellow", "red"]
+    legend_handles = [Line2D([0], [0], marker='o', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
+    axs5[idx].legend(legend_handles, legend_labels, loc='upper right')
+    axs5[idx].set_title(f"{feature}")
+    axs5[idx].set_xlabel("Beta")
+    axs5[idx].set_ylabel("Alpha")
+ 
 
 fig1.tight_layout()
 fig2.tight_layout()
 fig3.tight_layout()
 fig5.tight_layout()
-plt.show()
 
 if plot_hr == "Yes":
     fig6, axs6 = plt.subplots(4, 4)
@@ -291,5 +319,182 @@ if plot_hr == "Yes":
     handles, labels = axs6[i].get_legend_handles_labels()
     fig6.legend(handles, labels, loc='lower right')
     fig6.suptitle("Heart rate trend")
+    fig6.tight_layout()
 
-        
+#-------------------------------------------------------------------------------------------------------------------------------
+    # Test coefficients
+#-------------------------------------------------------------------------------------------------------------------------------
+
+   # Define the plot creation method
+#---------------------------------------------------------------------------------------------------------------------------------------------
+class TestModel():
+    def __init__(self):
+        pass
+
+    def plot_graph(self, coeff, fig, axs):
+        for i, ID in enumerate(participants):
+            ID = f"{ID:03}"
+
+            # Original data
+            data_hc = pd.read_excel(f"{path}\\Input to models\\Power Output models\\{ID}_Input_handcycle_{mode}.xlsx", usecols = ["Power"])
+            data_hc = data_hc.iloc[int(len(data_hc)/2):]
+            data_bc = pd.read_excel(f"{path}\\Input to models\\Power Output models\\{ID}_Input_bicycle_{mode}.xlsx", usecols = ["Power"])
+            data_bc = data_bc.iloc[int(len(data_bc)/2):]
+
+            # Corrected output
+            predicted_bc = data_hc * coeff[i, 0] + coeff[i, 1]
+            axs[i].plot(predicted_bc, color = "orange")
+            axs[i].plot(data_bc, color = "blue")
+            axs[i].set_title(f"Participant {ID}")
+            axs[i].set_xlabel("Time [s]")
+            axs[i].set_ylabel("Power [W]")
+
+            RPE_hc = members[f"{ID}"]["RPE"][0]
+            RPE_bc = members[f"{ID}"]["RPE"][1]
+
+            legend_labels = [str(RPE_hc), str(RPE_bc)]   
+            legend_colors = ["orange", "blue"] 
+            legend_handles = [Line2D([0], [0], marker='.', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
+            axs[i].legend(legend_handles, legend_labels, loc='upper left', prop={'size': 6})
+            
+            legend_labels = ["Tweaked handcycle", "Original bicycle"]
+            fig.legend(legend_handles, legend_labels, loc='lower right')
+test_model = TestModel()
+
+#-------------------------------------------------------------------------------------------------------------------------------------------
+    # Model with the original coefficients
+#-------------------------------------------------------------------------------------------------------------------------------------------
+slopes_model = slopes_model.reshape(-1, 1)
+intercepts_model = intercepts_model.reshape(-1, 1)
+
+fig7, axs7 = plt.subplots(4, 4)
+axs7 = axs7.flatten()
+coef_orig = np.concatenate([slopes_model, intercepts_model], axis = 1)
+test_model.plot_graph(coef_orig, fig7, axs7)
+fig7.suptitle("Tweaked Power output")
+fig7.tight_layout()
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+    # Multioutput regression (Same results as computing slopes and intercept separately)
+#--------------------------------------------------------------------------------------------------------------------------------------------
+fig8, axs8 = plt.subplots(2)
+coef_predicted = np.zeros((len(participants), 2))
+
+for i, ID in enumerate(participants):
+    ID = f"{ID:03}"
+
+    training = [
+    [sub_dict[key] for key in ["Age", "Height", "Weight", "Gender"] if key in sub_dict]
+    for key, sub_dict in members.items() if key != ID]
+    test = [
+    [sub_dict[key] for key in ["Age", "Height", "Weight", "Gender"] if key in sub_dict]
+    for key, sub_dict in members.items() if key == ID]
+
+    Y_train = np.delete(np.concatenate([slopes_model, intercepts_model], axis = 1), i, axis = 0)
+
+    # Linear regression
+    linear_regression = LinearRegression().fit(training, Y_train)
+    linear_model = linear_regression.predict(test)
+    coef_predicted[i, :] = linear_model[0, :]
+
+    axs8[0].scatter(i, linear_model[0, 0], color = "red")
+    axs8[1].scatter(i, linear_model[0, 1], color = "red")
+    axs8[0].scatter(i, slopes_model[i], color = "blue")
+    axs8[1].scatter(i, intercepts_model[i], color = "blue")
+
+    legend_labels = ["Predicted", "Original"]   
+    legend_colors = ["red", "blue"] 
+    legend_handles = [Line2D([0], [0], marker='.', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
+    axs8[0].legend(legend_handles, legend_labels, loc='upper left', prop={'size': 6})
+    axs8[1].legend(legend_handles, legend_labels, loc='upper left', prop={'size': 6})
+    
+   # Test the predicted coefficients
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+fig9, axs9 = plt.subplots(4, 4)
+axs9 = axs9.flatten()
+test_model.plot_graph(coef_predicted, fig9, axs9)
+fig9.suptitle("Linear model: tweaked Power output")
+
+    # Compare Original model and Linear Model
+#-----------------------------------------------------------------------------------------------------------------------------------------------------
+fig10, axs10 = plt.subplots(4, 4)
+axs10 = axs10.flatten()
+
+for i, ID in enumerate(participants):
+    ID = f"{ID:03}"
+
+    # Original data
+    data_hc = pd.read_excel(f"{path}\\Input to models\\Power Output models\\{ID}_Input_handcycle_{mode}.xlsx", usecols = ["Power"])
+    data_hc = data_hc.iloc[int(len(data_hc)/2):]
+
+    # Corrected output
+    predicted = data_hc * coef_predicted[i, 0] + coef_predicted[i, 1]
+    orig = data_hc * coef_orig[i, 0] + coef_orig[i, 1]
+    axs10[i].plot(predicted, color = "orange")
+    axs10[i].plot(orig, color = "blue")
+    axs10[i].set_title(f"Participant {ID}")
+    axs10[i].set_xlabel("Time [s]")
+    axs10[i].set_ylabel("Power [W]")
+
+    RPE_hc = members[f"{ID}"]["RPE"][0]
+    RPE_bc = members[f"{ID}"]["RPE"][1]
+
+    legend_labels = [str(RPE_hc), str(RPE_bc)]   
+    legend_colors = ["orange", "blue"] 
+    legend_handles = [Line2D([0], [0], marker='.', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
+    axs10[i].legend(legend_handles, legend_labels, loc='upper left', prop={'size': 6})
+    
+    legend_labels = ["Linear regression", "Original coefficients"]
+    fig10.legend(legend_handles, legend_labels, loc='lower right')
+
+plt.show()
+    
+    
+if simple_model == "Yes":
+    fig11, axs11 = plt.subplots(4, 4)
+    axs11 = axs11.flatten()
+
+    for i, ID in enumerate(participants):
+        alpha = 0
+        ID = ID = f"{ID:03}"
+
+        # Import data
+        data_hc = pd.read_excel(f"{path}\\Input to models\\Power Output models\\{ID}_Input_handcycle_{mode}.xlsx", usecols = ["Power", "RPE"])
+        data_hc_RPE = data_hc.iloc[:int(len(data_hc)/2)] # Blocks at constant RPE target
+        data_hc_power = data_hc.iloc[int(len(data_hc)/2):] # Blocks at constant power target
+        data_bc = pd.read_excel(f"{path}\\Input to models\\Power Output models\\{ID}_Input_bicycle_{mode}.xlsx", usecols = ["Power", "RPE"])
+        data_bc_RPE = data_bc.iloc[:int(len(data_hc)/2)] # Blocks at constant RPE target
+        data_bc_power = data_bc.iloc[int(len(data_hc)/2):] # Blocks at constant power target
+
+        alpha = alpha + np.mean(data_bc_RPE.loc[data_bc_RPE["RPE"] == 12, :])/np.mean(data_hc_RPE.loc[data_hc_RPE["RPE"] == 12, :])
+        alpha = alpha + np.mean(data_bc_RPE.loc[data_bc_RPE["RPE"] == 14, :])/np.mean(data_hc_RPE.loc[data_hc_RPE["RPE"] == 14, :])
+        alpha = alpha + np.mean(data_bc_RPE.loc[data_bc_RPE["RPE"] == 15, :])/np.mean(data_hc_RPE.loc[data_hc_RPE["RPE"] == 15, :])
+
+        alpha = alpha / 3
+
+        Predicted_output = alpha * data_hc_power["Power"]
+
+        axs11[i].plot(data_bc_power["Power"])
+        axs11[i].plot(Predicted_output)
+
+        axs11[i].set_xlabel("Time [s]")
+        axs11[i].set_ylabel("Power [W]")
+
+
+        axs11[i].set_title(f"{ID}: alpha = {alpha:.2f}")
+        legend_labels = ["Original bicycle", "Predicted value"]
+        legend_colors = ["Blue", "Orange"]
+        legend_handles = [Line2D([0], [0], marker='.', color='w', markerfacecolor=color, markersize=10) for color in legend_colors]
+        fig11.legend(legend_handles, legend_labels, loc='lower right')
+
+    fig11.tight_layout()
+    fig11.suptitle("Simple model")
+
+
+
+
+
+    
+
+
+
